157211fb8bd33dae61829f8df1a2a8d9ef9b3b72
loutix <62959342+loutix@users.noreply.github.com>
2024-03-08 17:34:57 +0100

    actuator

--- src/main/java/com/ocrooms/safetynet/SafetyNetApplication.java
+++ src/main/java/com/ocrooms/safetynet/SafetyNetApplication.java
@@ -1,7 +1,9 @@
 package com.ocrooms.safetynet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SafetyNetApplication {
@@ -9,6 +11,9 @@ public class SafetyNetApplication {
     public static void main(String[] args) {
         SpringApplication.run(SafetyNetApplication.class, args);
     }
-
+    @Bean
+    public InMemoryHttpExchangeRepository createTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 
 }
